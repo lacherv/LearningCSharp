5a85961baedb616a1a61bfbc46e98952d38f7900
Christian Awelakoue <awelakoue@gmail.com>
2018-04-16 23:14:21 +0200

    Looping Counting

---
+++ LoopCounting/LoopCounting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoopCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var count = 1;
+            while (count <= 5)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine($"========{count}============");
+                for (int k = 10; k > 0; k--)
+                {
+                    Console.WriteLine(k);
+                }
+                Console.WriteLine($"========{count}============");
+                count++;
+            }
+            
+        }
+    }
+}
