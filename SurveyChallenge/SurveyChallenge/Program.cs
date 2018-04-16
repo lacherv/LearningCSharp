1dd28c994e37aed09aa33c7c9d48b3c734e7c929
Christian Awelakoue <awelakoue@gmail.com>
2018-04-16 21:37:34 +0200

    first challenge

---
+++ SurveyChallenge/SurveyChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Challenge: Multiple Choice Questions 
+ * Create a simple three-question survey
+ * Output the answers back in the console
+ */
+using System;
+
+namespace SurveyChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name: ");
+            var yourName = Console.ReadLine();
+            Console.Write("How old are you? ");
+            var yourAge = Console.ReadLine();
+            Console.Write("What month were you born in? ");
+            var birthMonth = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Your name is {yourName}, you are {yourAge} years old.\nAnd you are born in {birthMonth}.");
+            Console.ReadKey();
+        }
+    }
+}
