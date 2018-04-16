c898b358ef1cb37c0d21af93605c32def5a276e1
Christian Awelakoue <awelakoue@gmail.com>
2018-04-16 21:48:12 +0200

    conditional if

---
+++ Survey/Survey/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Survey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            var age = Console.ReadLine();
+
+            Console.WriteLine("What month were you born in?");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Your name is : {0}", name); // place holder
+            Console.WriteLine("Your age is : {0}", age); // place holder
+            Console.WriteLine("Your birth month is : {0}", month); // place holder
+
+            if (month == "march")
+            {
+                Console.WriteLine("You are an Aries");
+            }
+            else if (month == "april")
+            {
+                Console.WriteLine("You are a Taurus");
+            }
+            else if (month == "may")
+            {
+                Console.WriteLine("You are a Gemini");
+            }
+        }
+    }
+}
